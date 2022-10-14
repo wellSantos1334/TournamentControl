0dcca7432f9603f3659bd91368dd9cd8257ead31
wellSantos1334 <103972693+wellSantos1334@users.noreply.github.com>
2022-10-13 23:28:55 -0300

    feat: EditTeams
    front-end and back-end to edit any team

--- controllers/TeamController.js
+++ controllers/TeamController.js
@@ -2,9 +2,8 @@ const Team = require('../models/Team')
 
 module.exports = class TeamController {
     static async createTeam(req, res) {
-        const Teams = await Team.findAll({order: [['id', 'DESC']], raw: true})
-        console.log(Teams)
-        res.render('team/createTeam', {Teams})
+        const Teams = await Team.findAll({ order: [['id', 'DESC']], raw: true })
+        res.render('team/createTeam', { Teams })
     }
 
     static async createTeamSave(req, res) {
@@ -20,4 +19,22 @@ module.exports = class TeamController {
         }
     }
 
+    static async editTeam(req, res) {
+        try {
+            const teamData = await Team.findAll({ where: { id: req.params.id }, raw: true })
+            res.render('team/editTeam', { teamData })
+        } catch (err) {
+            console.log("Error: " + err)
+        }
+
+    }
+
+    static async editTeamSave(req, res) {
+        try {
+            const teamData = await Team.update({ name: req.body.name }, { where: { id: req.body.id } })
+            res.redirect('createTeam')
+        } catch (err) {
+            console.log("Error: "+err)
+        }
+    }
 }
--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -6,5 +6,7 @@ const TeamController = require('../controllers/TeamController')
 
 router.get('/createTeam', TeamController.createTeam)
 router.post('/createTeamSave', TeamController.createTeamSave)
+router.get('/editTeam/:id', TeamController.editTeam)
+router.post('/editTeamSave', TeamController.editTeamSave)
 
 module.exports = router
